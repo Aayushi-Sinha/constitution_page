40519a73fb5af27aeb8f01ce22feb720e0e75dfa
Aayushi-Sinha <aayushisinha2707@gmail.com>
2024-09-10 17:51:08 +0530

    Links

--- constitution_page/src/App.js
+++ constitution_page/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import Glossary from './components/glossary';
 import RouteMap from './components/RouteMap';
 import Index from './components/index';
+// import Chakra from './components/chakra'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/glossary" element={<Glossary />} />
         <Route path="/preamble" element={ <RouteMap/>} />
         <Route path="/index" element={ <Index/>} />
+        {/* <Route path="/themes" element={ <Chakra/>} /> */}
 
       </Routes>
     </Router>
